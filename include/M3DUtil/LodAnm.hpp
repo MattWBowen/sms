224494fba295c8ec4680ee4af888f8276f4900ab
Matt Bowen <username8622@gmail.com>
2025-10-27 22:51:29 -0700

    LodAnm attempt 1

--- include/M3DUtil/LodAnm.hpp
+++ include/M3DUtil/LodAnm.hpp
@@ -2,19 +2,30 @@
 #define M3DUTIL_LOD_ANM_HPP
 
 class TLiveActor;
+struct TLodAnmIndex { 
+	int index; //probably wrong?
 
-struct TLodAnmIndex { };
+	int data[3]; //TBD, this is just to make the struct the right size
+}; 
+
 
 class TLodAnm {
 public:
-	TLodAnm(TLiveActor*, const TLodAnmIndex*, int, float);
-	void execChangeLod();
-	void setBckAndBtpAnm(int);
-	void setBtpAnm_(int);
-	void setBckAnm_(int);
+    TLodAnm(TLiveActor* param_1, const TLodAnmIndex* param_2, int param_3, float param_4);
+    
+    void execChangeLod();
+    void setBckAndBtpAnm(int);
+    void setBtpAnm_(int);
+    void setBckAnm_(int);
 
-public:
-	/* 0x0 */ char unk0[0x20];
+    /* 0x0 */ TLiveActor* mActor;     
+    /* 0x04 */const TLodAnmIndex* mIndex;     
+    
+	/* 0x08 */int mParam3;                    
+    /* 0x0c */float mParam4;                 
+    /* 0x10 */int mField10;                 
+    /* 0x14 */int mField14;             
+    /* 0x18 */int mField18;                
+    /* 0x1c */int mField1C;             
 };
-
 #endif
--- src/M3DUtil/LodAnm.cpp
+++ src/M3DUtil/LodAnm.cpp
@@ -1,9 +1,35 @@
 #include <M3DUtil/LodAnm.hpp>
 
-TLodAnm::TLodAnm(TLiveActor*, const TLodAnmIndex*, int, float) { }
+//bad: slwi r0, r0, 2      good: //slwi r0, r0, 4     me: 
+TLodAnm::TLodAnm(TLiveActor* param_1, const TLodAnmIndex* param_2, int param_3, float param_4) {
+    mActor = param_1;
+    mIndex = param_2;
+    mParam3 = param_3;
+    mParam4 = param_4;
+
+    mField10 = 0;
+    mField14 = -1;
+    mField18 = 0;
+    mField1C = 0;
+    
+    if (param_2 != 0) {
+
+        while (param_2[mField10].index >= -1) {
+            mField10++;
+        }
+    }
+} 
 
 void TLodAnm::setBckAnm_(int) { }
 
-void TLodAnm::setBtpAnm_(int) { }
+void TLodAnm::setBtpAnm_(int param_1) { }
+void TLodAnm::setBckAndBtpAnm(int param_1) 
+{ 
+    //TODO
 
-void TLodAnm::setBckAndBtpAnm(int) { }
+    setBckAnm_(param_1);
+    if (mIndex != 0)
+    {
+        setBtpAnm_(param_1);
+    }
+}
